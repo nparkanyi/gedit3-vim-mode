3a91aa4b120b2c477d4d1faef0b3df573aebef51
PocketEngi <pocketengi@gmail.com>
2015-03-24 14:02:06 -0400

    Added .gitignore and fixed pep8 violations

--- vim-mode.py
+++ vim-mode.py
@@ -1,51 +1,52 @@
-#gedit3-vim-mode: Vim-style keyboard navigation plugin for gedit3.
-#Copyright © 2015 Nicholas Parkanyi, licensed under GPLv3 (see 'gpl.txt')
+# gedit3-vim-mode: Vim-style keyboard navigation plugin for gedit3.
+# Copyright © 2015 Nicholas Parkanyi, licensed under GPLv3 (see 'gpl.txt')
 from gi.repository import GObject, Gedit
 
 class VimMode(GObject.Object, Gedit.ViewActivatable):
   __gtype_name__ = "VimMode"
   
   view = GObject.property(type=Gedit.View)
-  
+
   def __init__(self):
     GObject.Object.__init__(self)
     self.block = False
     
   def callback(self, widget, event):
-    if (event.keyval == 0xff1b):
+    if event.keyval == 0xff1b:
       self.block = not self.block  
-    elif (event.keyval == 0x020 and self.block):
+    elif event.keyval == 0x020 and self.block:
       buf = self.view.get_buffer()
       print(buf.props.cursor_position)
       it = buf.get_start_iter()
       buf.place_cursor(it) 
-    #'i' insert mode
-    elif (event.keyval == 0x069 and self.block):
+    #  'i' insert mode
+    elif event.keyval == 0x069 and self.block:
       self.block = False
       return True
-    #'j' cursor down
-    elif (event.keyval ==  0x06a and self.block):
+    elif self
+    # 'j' cursor down
+    elif event.keyval == 0x06a and self.block:
       buf = self.view.get_buffer()
       it = buf.get_start_iter()
       it.set_offset(buf.props.cursor_position)
       it.forward_line()    
       buf.place_cursor(it)
-    #'k' cursor up
-    elif (event.keyval == 0x06b and self.block):
+    # 'k' cursor up
+    elif event.keyval == 0x06b and self.block:
       buf = self.view.get_buffer()
       it = buf.get_start_iter()
       it.set_offset(buf.props.cursor_position)
       it.backward_line()
       buf.place_cursor(it)
-    #'l' cursor right
-    elif (event.keyval == 0x06c and self.block):
+    # 'l' cursor right
+    elif event.keyval == 0x06c and self.block:
       buf = self.view.get_buffer()
       it = buf.get_start_iter()
       it.set_offset(buf.props.cursor_position)
       it.forward_char()
       buf.place_cursor(it)
-    #'h' cursor left
-    elif (event.keyval == 0x068 and self.block):
+    # 'h' cursor left
+    elif event.keyval == 0x068 and self.block:
       buf = self.view.get_buffer()
       it = buf.get_start_iter()
       it.set_offset(buf.props.cursor_position)
