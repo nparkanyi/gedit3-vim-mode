b657f561584543465e867c1655f7242b75396cc9
Nicholas Parkanyi <n.parkanyi@gmail.com>
2015-06-14 21:59:51 -0400

    utf8 encoding line

--- vim-mode.py
+++ vim-mode.py
@@ -1,3 +1,4 @@
+# -*- coding: utf-8 -*-
 # gedit3-vim-mode: Vim-style keyboard navigation plugin for gedit3.
 # Copyright 2015 Nicholas Parkanyi, licensed under GPLv3 (see 'gpl.txt')
 from gi.repository import GObject, Gedit, Gdk
